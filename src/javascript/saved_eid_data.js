d82d9407b860992721ebb68682b05e81fe1a4786
Edwin <edwin@DYYQ0CVWKV.fritz.box>
2023-04-25 15:52:34 +0100

    Verifying that validate errors are working

--- src/javascript/saved_eid_data.js
+++ src/javascript/saved_eid_data.js
@@ -3,12 +3,13 @@ module.exports.eidId = 'eid:ec984830-3007-11ec-a72f-7b66a29ade54';
 module.exports.eidType = 'eid';
 module.exports.savedEidPayload = {
   attributes: {
-    title: "SDK created EID",
+    title: "SDK EID",
     version: 10,
     siren: {
       ui: {
         icon: "fas fa-circle",
-        color: "#333333"
+        color: "#333333",
+        shortDescription: ""
       }
     }
   },
--- src/javascript/with_security_with_ssl_with_basepath.js
+++ src/javascript/with_security_with_ssl_with_basepath.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const https = require('node:https');
 
-const Openapi = require('../../../temp/javascript/dist/index');
-const client = new Openapi.ApiClient('https://localhost:5606/wox'); // update this basepath for dev mode
+const Openapi = require('../../../master/temp/javascript/dist/index');
+const client = new Openapi.ApiClient('https://localhost:5606/fui'); // update this basepath for dev mode
 
 const {
   parentSavedSearchPayload,
@@ -28,16 +28,15 @@ client.authentications = {
 }
 
 const options = {
-  ca: fs.readFileSync('/home/edwin/work/kibi-internal/pki/cacert.pem')
+  ca: fs.readFileSync('/Users/edwin/work/master/kibi-internal/pki/cacert.pem')
 }
 client.requestAgent = new https.Agent(options);
-
 const api = new Openapi.ApiVersionV1Api(client)
 
 const callback = function (error, data, response) {
   console.log('--------------- RESULT OF API CALL -------------------');
-  if (response.error) {
-    console.error(response.error.status, response.error.text);
+  if (error.response) {
+    console.log(error.status, JSON.parse(error.response.text));
   } else if (response.body.warning) {
     console.log('API called successfully with status ' + response.status + ' . But there was a warning: "' + response.body.warning + '"');
   } else {
@@ -48,8 +47,8 @@ const callback = function (error, data, response) {
 
 const callbackForValidate = function (error, data, response) {
   console.log('--------------- RESULT OF VALIDATE API CALL -------------------');
-  if (response.error) {
-    console.error(response.error.status, response.error.text);
+   if (error.response) {
+    console.log(error.status, JSON.parse(error.response.text));
   } else if (response.body.warning) {
     console.log('API validated the ' + response.body.type + ' object successfully with status: ' + response.status + '. But there was a warning: "' + response.body.warning + '"');
   } else {
@@ -59,19 +58,22 @@ const callbackForValidate = function (error, data, response) {
 
 // Saved Search
 // here we can specify an id, which is required for parent id in order to link it to the child search
-// api.updateInvestigateObject(type, parentSavedSearchId, parentSavedSearchPayload, { overwrite: true }, callback)
+api.updateInvestigateObject(searchType, parentSavedSearchId, parentSavedSearchPayload, callback)
 
-// here we validate and create a child search, we have a concrete id from the above call
-// api.validateInvestigateObject(searchType, parentSavedSearchPayload, callbackForValidate);
-// api.validateInvestigateObject(searchType, childSavedSearchPayload, callbackForValidate);
+// creating objects 
+api.createInvestigateObject(eidType, savedEidPayload, callback)
+api.createInvestigateObject(searchType, childSavedSearchPayload, callbackForValidate); // we have a concrete id from the above call
 
+// validating various objects
+api.validateInvestigateObject(searchType, parentSavedSearchPayload, callbackForValidate);
+api.validateInvestigateObject(searchType, childSavedSearchPayload, callbackForValidate);
+api.validateInvestigateObject(eidType, savedEidPayload, callbackForValidate);
 
-// api.createInvestigateObjectWithId(searchType, parentSavedSearchId, parentSavedSearchPayload, { overwrite: false }, callback)
-
-// creating an investigate object
-// api.createInvestigateObject(type, childSavedSearchPayload, callback)
-
-// savedEids
-// api.createInvestigateObject(eidType, savedEidPayload, callback)
-// api.validateInvestigateObject(eidType, savedEidPayload, callbackForValidate)
+// create investigate object with Id
+api.createInvestigateObjectWithId(searchType, parentSavedSearchId, parentSavedSearchPayload, { overwrite: false }, callback)
 api.createInvestigateObjectWithId(eidType, eidId, savedEidPayload, { overwrite: true }, callback)
+
+// failed validations based on wrong type being passed to request
+api.validateInvestigateObject(eidType, parentSavedSearchPayload, callbackForValidate);
+api.validateInvestigateObject(searchType, savedEidPayload, callbackForValidate);
+api.validateInvestigateObject(eidType, childSavedSearchPayload, callbackForValidate);
